10eca359c23f7ddc478ce8c5d924c857af389dbb
dgstevens1 <stevens.dg.1@gmail.com>
2018-09-08 11:06:01 -0400

    added draw_bs_pairs_linreg

--- data_sciences_defs.py
+++ data_sciences_defs.py
@@ -78,3 +78,43 @@ def permutation_sample(data1, data2):
     perm_sample_2 = permuted_data[len(data1):]
 
     return perm_sample_1, perm_sample_2
+
+def draw_perm_reps(data_1, data_2, func, size=1):
+    """Generate multiple permutation replicates."""
+
+    # Initialize array of replicates: perm_replicates
+    perm_replicates = np.empty(size)
+
+    for i in range(size):
+        # Generate permutation sample
+        perm_sample_1, perm_sample_2 = permutation_sample(data_1,data_2)
+
+        # Compute the test statistic
+        perm_replicates[i] = func(perm_sample_1, perm_sample_2)
+
+    return perm_replicates
+
+def diff_of_means(data_1, data_2):
+    #Difference in means of two arrays
+    # The difference of means of data_1, data_2: diff
+    diff = np.mean(data_1) - np.mean(data_2)
+
+    return diff
+
+def draw_bs_pairs_linreg(x, y, size=1):
+    """Perform pairs bootstrap for linear regression."""
+
+    # Set up array of indices to sample from
+    inds = np.arange(len(x))
+
+    # Initialize samples
+    bs_slope_reps = np.empty(size)
+    bs_intercept_reps = np.empty(size)
+
+    # Take samples
+    for i in range(size):
+        bs_inds = np.random.choice(inds, len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x, bs_y, 1)
+
+    return bs_slope_reps, bs_intercept_reps
